dfab4e8f39f2aff51c83d39abb6a0a84db52b23d
JoseRoj <jorojas2019@udec.cl>
2023-08-27 00:20:42 -0400

    Añadir test multiplos de 3

--- TDD_FizzBuzz/test/test_fb.py
+++ TDD_FizzBuzz/test/test_fb.py
@@ -5,3 +5,7 @@ def test_normal():
     for i in [1, 2, 4, 7]:
         assert fizz_buzz(i) == i
 
+def test_mult3():
+    assert fizz_buzz(3) == "fizz"
+    assert fizz_buzz(6) == "fizz"  
+    assert fizz_buzz(9) == "fizz"
