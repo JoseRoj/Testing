ca8f565665174ea29fae8ac432dcd3ce7a9e7b33
JoseRoj <jorojas2019@udec.cl>
2023-08-27 00:29:44 -0400

    Añadir test para Buzz

--- TDD_FizzBuzz/test/test_fb.py
+++ TDD_FizzBuzz/test/test_fb.py
@@ -8,3 +8,7 @@ def test_normal():
 def test_mult3():
     for i in [3, 6, 9]:
         assert fizz_buzz(i) == "fizz"
+
+def test_mult5():
+    for i in [5, 10]:
+        assert fizz_buzz(i) == "buzz"
