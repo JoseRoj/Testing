506b1e93a636fd338f6a6df37756d783d6f58aa9
JoseRoj <jorojas2019@udec.cl>
2023-08-27 01:29:25 -0400

    Refactorización de la solución final

--- TDD_FizzBuzz/app/fizzbuzz.py
+++ TDD_FizzBuzz/app/fizzbuzz.py
@@ -1,9 +1,8 @@
 def fizz_buzz(number):
-    if number%3 == 0 and number%5 == 0:
-        return "fizzbuzz"
-    if number%3 == 0:
-        return "fizz"
     if number%5 == 0:
+        if number%3 == 0:
+            return "fizzbuzz"
         return "buzz"
-
+    elif number%3 == 0:
+        return "fizz"
     return number
