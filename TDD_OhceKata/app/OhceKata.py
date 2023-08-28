44de7c8fc815bcc9cc587d74de29c6466d3b8487
JoseRoj <jorojas2019@udec.cl>
2023-08-27 22:57:14 -0400

    Agregar implementación para cada uno de los test

--- TDD_OhceKata/app/OhceKata.py
+++ TDD_OhceKata/app/OhceKata.py
@@ -1,2 +1,8 @@
 def saludar_noche(name):
     return "¡Buenas noches Pedro!"
+
+def saludar_tarde(name):
+    return "¡Buenas tardes Pedro!"
+
+def saludar_dia(name):
+    return "¡Buenos días Pedro!"
