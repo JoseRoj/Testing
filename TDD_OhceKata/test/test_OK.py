1a194ec587c46716798e6f004390006f93458398
JoseRoj <jorojas2019@udec.cl>
2023-08-27 22:56:33 -0400

    Agregar pruebas fallidas para cada opción saludos

--- TDD_OhceKata/test/test_OK.py
+++ TDD_OhceKata/test/test_OK.py
@@ -2,3 +2,9 @@ import pytest
 from app.OhceKata import *
 def test_saludar_noche():
     assert saludar_noche("Pedro") == "¡Buenas noches Pedro!"
+
+def test_saludar_tarde():
+    assert saludar_tarde("Pedro") == "¡Buenas tardes Pedro!"
+
+def test_saludar_dia():
+    assert saludar_dia("Pedro") == "¡Buenos días Pedro!"
