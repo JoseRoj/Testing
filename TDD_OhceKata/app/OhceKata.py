5578f577fa6659e594096a2c80f04b1647c6c1a8
JoseRoj <jorojas2019@udec.cl>
2023-08-27 23:50:57 -0400

    Agr. implementación para test agregados de saludos

--- TDD_OhceKata/app/OhceKata.py
+++ TDD_OhceKata/app/OhceKata.py
@@ -1,8 +1,12 @@
-def saludar_noche(name):
-    return "¡Buenas noches Pedro!"
+import datetime
+def saludar_noche(name,hora = datetime.datetime.now().hour):
+    if hora == 20 or hora == 6:
+        return "¡Buenas noches " + name + "!"
 
-def saludar_tarde(name):
-    return "¡Buenas tardes Pedro!"
+def saludar_tarde(name,hora = datetime.datetime.now().hour):
+    if hora == 12 or hora == 18:
+        return "¡Buenas tardes " + name + "!"
 
-def saludar_dia(name):
-    return "¡Buenos días Pedro!"
+def saludar_dia(name,hora = datetime.datetime.now().hour):
+    if hora == 10 or hora == 8:
+        return "¡Buenos días " + name + "!"
