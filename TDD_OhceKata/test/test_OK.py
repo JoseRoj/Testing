0b6dad8f21a5da0a4361a76439813f6c648c3e9e
JoseRoj <jorojas2019@udec.cl>
2023-08-27 23:47:29 -0400

    Agregando nuevos casos de prueba fallida

--- TDD_OhceKata/test/test_OK.py
+++ TDD_OhceKata/test/test_OK.py
@@ -1,10 +1,15 @@
 import pytest
 from app.OhceKata import *
 def test_saludar_noche():
-    assert saludar_noche("Pedro") == "¡Buenas noches Pedro!"
+    assert saludar_noche("Pedro",20) == "¡Buenas noches Pedro!"
+    assert saludar_noche("Ale",22) == "¡Buenas noches Ale!"
 
 def test_saludar_tarde():
-    assert saludar_tarde("Pedro") == "¡Buenas tardes Pedro!"
+    assert saludar_tarde("Pedro",12) == "¡Buenas tardes Pedro!"
+    assert saludar_tarde("Ale",18) == "¡Buenas tardes Ale!"
 
 def test_saludar_dia():
-    assert saludar_dia("Pedro") == "¡Buenos días Pedro!"
+    assert saludar_dia("Pedro",10) == "¡Buenos días Pedro!"
+    assert saludar_dia("Ale",8) == "¡Buenos días Ale!"    
+    
+   
