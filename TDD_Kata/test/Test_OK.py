e6053e6a4e3c1b880455b76184faf1e24057a135
JoseRoj <jorojas2019@udec.cl>
2023-08-30 20:33:20 -0400

    Agr. nuevos test para saludar_noche

--- TDD_Kata/test/Test_OK.py
+++ TDD_Kata/test/Test_OK.py
@@ -2,4 +2,6 @@ import pytest
 from app.Ohce import *
 
 def test_saludar_noche():
-    assert saludar("Pedro") == "¡Buenas noches Pedro!"
+    assert saludar("Pedro",20) == "¡Buenas noches Pedro!"
+    assert saludar("Kel",3) == "¡Buenas noches Kel!"
+    assert saludar("Juan",5) == "¡Buenas noches Juan!"
