d3610b87f3ed1ef2c83d0889c43c8b8fb07983ae
JoseRoj <jorojas2019@udec.cl>
2023-08-30 20:39:52 -0400

    Refactorizar implementación saludar

--- TDD_Kata/app/Ohce.py
+++ TDD_Kata/app/Ohce.py
@@ -1,7 +1,7 @@
 def saludar(name,hora):
     if hora >= 20 or hora < 6:
         return "¡Buenas noches " + name + "!"
-    if hora >= 6 and hora < 12:
+    elif hora >= 6 and hora < 12:
         return "¡Buenos días " + name + "!"
-    if hora >= 12 and hora < 20:
+    else: 
         return "¡Buenas tardes " + name + "!"
