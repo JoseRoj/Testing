f592031a68c32e744389234196388e630124380e
JoseRoj <jorojas2019@udec.cl>
2023-08-30 20:35:11 -0400

    Agr. test fallido para saludar_dia

--- TDD_Kata/test/Test_OK.py
+++ TDD_Kata/test/Test_OK.py
@@ -5,3 +5,8 @@ def test_saludar_noche():
     assert saludar("Pedro",20) == "¡Buenas noches Pedro!"
     assert saludar("Kel",3) == "¡Buenas noches Kel!"
     assert saludar("Juan",5) == "¡Buenas noches Juan!"
+
+def test_saludar_dia():
+    assert saludar("Pedro",7) == "¡Buenos días Pedro!"
+    assert saludar("Ale",9) == "¡Buenos días Ale!"
+    assert saludar("Juan",11) == "¡Buenos días Juan!"
